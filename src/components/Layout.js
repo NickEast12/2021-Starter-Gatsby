d0f52d95c66428fadebd0bb2f47241caa344669f
Nick <nickeast1998@gmail.com>
2021-07-23 11:46:00 +0100

    changes to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import 'normalize.css';
-import styles, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 //* Local imports
-import GlobalStyles from '../styles/GlobalStyles';
-import Nav from './Nav';
-import Footer from './Footer';
+import GlobalStyles from '../styles/globalstyles';
+import nav from './nav';
+import footer from './footer';
+import theme from '../styles/theme';
 
-// https://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
+// ? https://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
 if (typeof window !== 'undefined') {
   // eslint-disable-next-line global-require
   require('smooth-scroll')('a[href*="#"]');
 }
 
-const Layout = ({ children, alt }) => {
+function Layout({ children }) {
   const i = true;
   return (
     <>
-      <GlobalStyles />
-      <Nav />
-      <main>{children}</main>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <nav />
+        <main>{children}</main>
+        <footer />
+      </ThemeProvider>
     </>
   );
+}
+
+Layout.propTypes = {
+  children: PropTypes.any,
 };
 
 export default Layout;
